f8d4e84ac6ef5922af1b9e35084876cbc6c5a944
Hai Nguyen <crisredfiel2305@gmail.com>
2025-09-13 20:47:54 +0700

    p0050

---
+++ J1.S.P0050/src/main/java/j1/s/p0050/J1SP0050.java
@@ -0,0 +1,14 @@
+
+
+package j1.s.p0050;
+
+/**
+ *
+ * @author DuckHai
+ */
+public class J1SP0050 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

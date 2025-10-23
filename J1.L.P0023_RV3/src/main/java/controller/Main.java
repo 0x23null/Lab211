21cff6d59c6bb59fd0b4df46e1e5deb1f9b8c155
Hai Nguyen <crisredfiel2305@gmail.com>
2025-10-23 23:04:00 +0700

    slot-6-2

--- J1.L.P0023_RV3/src/main/java/controller/Main.java
+++ J1.L.P0023_RV3/src/main/java/controller/Main.java
@@ -3,7 +3,10 @@ package controller;
 import model.FruitShopManager;
 
 public class Main {
-    FruitShopManager fsm = new FruitShopManager();
-    // fsm.run() tai sao khong duoc??
-            
+
+    public static void main(String[] args) {
+        FruitShopManager fsm = new FruitShopManager();
+        fsm.run();
+    }
+
 }
